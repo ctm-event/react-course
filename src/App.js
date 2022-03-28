6f4ed15c8eddc325a3d701e335002e1a3e882e93
Steven <steven.nguyen@contemi.com>
2022-03-28 17:22:38 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      title: 'Car Insurance',
+      amount: 984.23,
+      date: new Date(2022, 3, 12)
+    },
+    {
+      title: 'Food',
+      amount: 234.52,
+      date: new Date(2022, 4, 11)
+    },
+    {
+      title: 'Fish',
+      amount: 111.23,
+      date: new Date(2022, 6, 8)
+    },
+    {
+      title: 'Fuel',
+      amount: 333.23,
+      date: new Date(2022, 2, 1)
+    },
+    {
+      title: 'Traveling',
+      amount: 231.27,
+      date: new Date(2022, 9, 22)
+    }
+  ];
+
+  const expenseElms = [];
+  expenses.forEach(item => {
+    expenseElms.push(
+      
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!!</h2>
+      {expenses.map((item) => {
+        return (
+          <ExpenseItem
+            date={item.date}
+            amount={item.amount}
+            title={item.title}
+          />
+        );
+      })}
     </div>
   );
 }
