c2236fed4a907a033fc49735032a6a6300f226ca
Steven <steven.nguyen@contemi.com>
2022-04-14 17:31:23 +0700

    Lession 103

--- user-management/src/components/User.js
+++ user-management/src/components/User.js
@@ -1,31 +1,32 @@
-import { useState } from "react";
-import Card from "./UI/Card";
-import UserList from "./UserList";
-import NewUser from './NewUser'
+import { useState, Fragment } from 'react';
+import UserList from './UserList';
+import NewUser from './NewUser';
 
 const User = () => {
   const [users, setUsers] = useState([]);
 
   const addUserHandler = (newUser) => {
     console.log(newUser);
-    setUsers(prevState => {
-      return [...prevState, newUser]
-    })
-  }
+    setUsers((prevState) => {
+      return [...prevState, newUser];
+    });
+  };
 
   const deleteUserHandler = (specifiedUser) => {
     console.log(specifiedUser);
-    setUsers(prevUsers => {
-      return prevUsers.filter(user => {
-        return user.id.toString() !== specifiedUser.id.toString()
-      })
-    })
-  }
+    setUsers((prevUsers) => {
+      return prevUsers.filter((user) => {
+        return user.id.toString() !== specifiedUser.id.toString();
+      });
+    });
+  };
 
-  return <Card>
-    <NewUser onAddUser={addUserHandler} />
-    <UserList onDeleteUser={deleteUserHandler} users={users} />
-  </Card>
-}
+  return (
+    <Fragment>
+      <NewUser onAddUser={addUserHandler} />
+      <UserList onDeleteUser={deleteUserHandler} users={users} />
+    </Fragment>
+  );
+};
 
 export default User;
