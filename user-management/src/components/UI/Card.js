8c38d4ddaba25c6333f24602f3adc15b6e3e72d2
Steven <steven.nguyen@contemi.com>
2022-04-18 17:30:01 +0700

    lesson 108

--- user-management/src/components/UI/Card.js
+++ user-management/src/components/UI/Card.js
@@ -1,8 +1,9 @@
-import styles from './Card.module.css';
+import React from 'react';
+
+import classes from './Card.module.css';
 
 const Card = (props) => {
-  const className = styles.Card + ' ' + (props.className || '');
-  return <div onClick={props.onClick}  className={className}>{props.children}</div>;
+  return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
 };
 
 export default Card;
